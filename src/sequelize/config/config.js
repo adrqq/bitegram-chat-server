352ba3560a62e9cd3646f6af83405ac183558e25
Adrian <forever0754@gmail.com>
2023-11-14 16:27:00 +0100

    empt

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   DEV: {
-    url: 'postgres://postgres:181915smv@192.168.0.131:5432/postgres',
+    url: 'postgres://postgres:181915smv@10.138.0.102:5432/postgres',
     dialect: 'postgres',
     logging: false,
   },
--- src/socketio/user-sockets.js
+++ src/socketio/user-sockets.js
@@ -5,41 +5,41 @@ const UserService = require('../services/user-service');
 io.on('connection', (socket) => {
   console.log('connected user with id: ', socket.id);
 
-  // socket.on('addFriend', async (data) => {
-  //   const { userId, friendId } = data;
+  socket.on('addFriend', async (data) => {
+    const { userId, friendId } = data;
 
-  //   console.table({ userId, friendId });
-  //   console.table(socket.rooms);
+    console.table({ userId, friendId });
+    console.table(socket.rooms);
 
-  //   // socket.join(friendId);
-  //   // socket.join(userId);
+    // socket.join(friendId);
+    // socket.join(userId);
 
-  //   await UserService.sendFriendRequest(userId, friendId)
-  //     .then(() => {
-  //       // io.to(userId).emit('friendRequestSent', { userId, friendId });
+    await UserService.sendFriendRequest(userId, friendId)
+      .then(() => {
+        // io.to(userId).emit('friendRequestSent', { userId, friendId });
 
-  //       io.emit('friendRequestSent', { userId, friendId });
-  //     })
-  // });
-
-  // socket.on('acceptFriendRequest', async (data) => {
-  //   const { userId, friendId } = data;
-
-  //   console.table({ userId, friendId });
-
-  //   socket.join(userId);
-  //   socket.join(friendId);
-
-  //   const user = await UserService.acceptFriendRequest(userId, friendId);
-
-  //   io.to(userId).emit('friendRequestAccepted', { userId, friendId });
-  // });
-
-  socket.on('testSocket', async () => {
-    console.log('testSocket');
-
-    socket.broadcast.emit('testSocket', { message: 'testSocket' });
+        io.emit('friendRequestSent', { userId, friendId });
+      })
   });
+
+  socket.on('acceptFriendRequest', async (data) => {
+    const { userId, friendId } = data;
+
+    console.table({ userId, friendId });
+
+    socket.join(userId);
+    socket.join(friendId);
+
+    const user = await UserService.acceptFriendRequest(userId, friendId);
+
+    io.to(userId).emit('friendRequestAccepted', { userId, friendId });
+  });
+
+  // socket.on('testSocket', async () => {
+  //   console.log('testSocket');
+
+  //   socket.broadcast.emit('testSocket', { message: 'testSocket' });
+  // });
 });
 
 module.exports = { io };
